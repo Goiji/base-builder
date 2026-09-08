a7ac34da79e813843af49bf26fea84cdcc48f028
Goiji <96674001+Goiji@users.noreply.github.com>
2026-09-08 03:55:45 +0000

    feat(token): add Base ERC721 NFT template
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

---
+++ src/tokens/BaseERC721.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+/// @title BaseERC721
+/// @notice Enumerable NFT template with owner-controlled minting.
+contract BaseERC721 is ERC721, ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    uint256 private _nextId = 1;
+    string public baseURI;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory baseURI_
+    ) ERC721(name_, symbol_) Ownable(msg.sender) {
+        baseURI = baseURI_;
+    }
+
+    function mint(address to) external onlyOwner returns (uint256 tokenId) {
+        tokenId = _nextId++;
+        _safeMint(to, tokenId);
+    }
+
+    function setBaseURI(string memory uri) external onlyOwner {
+        baseURI = uri;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+}
